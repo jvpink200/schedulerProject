9504cdf4ccb3ff7831582ad6662464dcefd0b0ae
Lakita Gaither <lakitagaither@yahoo.com>
2021-06-16 08:47:06 -0500

    Workspace Setup

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React Project
+          Learn React
         </a>
       </header>
     </div>
