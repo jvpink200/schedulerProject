b59784d58aedb582fed3b3a79df71df095be575f
jvpink200 <jvillarr20@gmail.com>
2021-08-18 17:04:56 -0700

    updated home component

--- scheduler/src/components/Home.js
+++ scheduler/src/components/Home.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import peoplePlanning from '..//animatedImages/images/peoplePlanning.jpeg';
-import CardAnimationComponent from './CardAnimationComponent';
-import AnimatingItems from './animatedItems';
-import Version from './DemoVersionComponent';
+import React, { Component } from "react";
+import peoplePlanning from "..//animatedImages/images/peoplePlanning.jpeg";
+import CardAnimationComponent from "./CardAnimationComponent";
+import AnimatingItems from "./animatedItems";
+import Version from "./DemoVersionComponent";
 
 class Home extends Component {
   render() {
     return (
       <React.Fragment>
         <div className="text-center container">
-          <h2 className="pt-3 hero-text">Let's get your next project...finished and on time!</h2>
+          <h2 className="pt-3 hero-text">
+            Let's get your next project...finished and on time!
+          </h2>
           <img
             src={peoplePlanning}
-            alt='People planning with clocks and calendars animation'
-            className='hero-img img-fluid max-width: 1000px height: auto'
+            alt="People planning with clocks and calendars animation"
+            className="hero-img img-fluid"
           />
         </div>
-
-        <CardAnimationComponent />
         <Version />
         <AnimatingItems />
       </React.Fragment>
